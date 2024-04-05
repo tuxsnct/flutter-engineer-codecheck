c2a0eb166bcebb5fc142938747421e0e50c14d4e
Hideki Tanaka <work@tuxsnct.com>
2024-04-06 01:55:26 +0900

    仮のホーム画面を作成

--- lib/features/common/presentation/screens/home_screen.dart
+++ lib/features/common/presentation/screens/home_screen.dart
@@ -1,48 +1,53 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({required this.title, super.key});
-
-  final String title;
+  const HomeScreen({super.key});
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(24),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'GitHub Viewer',
+              key: const Key('title'),
+              style: Theme.of(context).textTheme.displayLarge,
+              textAlign: TextAlign.center,
+              overflow: TextOverflow.ellipsis,
+              maxLines: 2,
+            ),
+            const SizedBox(height: 32),
+            FilledButton.tonal(
+              key: const Key('search_button'),
+              onPressed: () {},
+              style: FilledButton.styleFrom(
+                minimumSize: const Size.fromHeight(90),
+              ),
+              child: SizedBox(
+                width: double.maxFinite,
+                child: Wrap(
+                  alignment: WrapAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      'Search',
+                      style: Theme.of(context).textTheme.headlineSmall,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    const Icon(Icons.search, size: 32),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
