e6c5f6a1b6b317bc7aec5ed33d28ebca10319582
Hideki Tanaka <work@tuxsnct.com>
2024-04-10 23:05:34 +0900

    RepositoryModelにdescriptionを追加

--- lib/features/common/infrastructure/models/repository_model.dart
+++ lib/features/common/infrastructure/models/repository_model.dart
@@ -9,12 +9,13 @@ class RepositoryModel with _$RepositoryModel {
   const factory RepositoryModel({
     required String name,
     @JsonKey(name: 'full_name') required String fullName,
-    required OwnerModel owner,
-    required String language,
     @JsonKey(name: 'stargazers_count') required int stargazersCount,
     @JsonKey(name: 'watchers_count') required int watchersCount,
     @JsonKey(name: 'forks_count') required int forksCount,
     @JsonKey(name: 'open_issues_count') required int openIssuesCount,
+    String? description,
+    String? language,
+    OwnerModel? owner,
   }) = _RepositoryModel;
 
   factory RepositoryModel.fromJson(Map<String, dynamic> json) =>
