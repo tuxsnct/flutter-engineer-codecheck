58dab3813fb271bdf82447b59c3abb78c9f1ed31
Hideki Tanaka <work@tuxsnct.com>
2024-04-10 22:56:53 +0900

    Riverpodを導入

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:github_viewer/config/router.dart';
 import 'package:go_router/go_router.dart';
 
 // coverage:ignore-start
 void main() {
   GoRouter.optionURLReflectsImperativeAPIs = true;
-  runApp(const GithubViewerApp());
+  runApp(ProviderScope(child: GithubViewerApp(router: router)));
 }
 // coverage:ignore-end
 
 class GithubViewerApp extends StatelessWidget {
-  const GithubViewerApp({super.key});
+  const GithubViewerApp({required this.router, super.key});
+
+  final GoRouter router;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      title: 'Flutter Demo',
+      title: 'Github Viewer',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
